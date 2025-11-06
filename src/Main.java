79f149d9feebde19d74c41b59bc9b49fa08cb67a
SaikoDiablo <vikkiti37@gmail.com>
2025-11-06 15:45:36 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,40 +2,39 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-     Scanner obj = new Scanner(System.in);
+     Scanner obj = new Scanner(System.in); //creating a scanner for user to be able to input data
 
-     System.out.println("Enter a first word: ");
+     System.out.println("Enter a first string: ");
      String s1 = obj.nextLine();
 
-     System.out.println("Enter a second word:");
+     System.out.println("Enter a second string:");
      String s2 = obj.nextLine();
-
-     String s3 = s1 + s2;
-     String s4 = s2 + s1;
+    //concatenation
+     String s3 = s1 + s2;//join frist string with second
+     String s4 = s2 + s1;// nhere join second with first
 
      
-     System.out.println("First + Second: " + s3);
-    System.out.println("Second + First: " + s4);
+     System.out.println("First + Second: " + s3);//printing result
+     System.out.println("Second + First: " + s4);
    
 
-     if (s1.equals(s2)){
-        System.out.println("Words are equal");
+     if (s1.equals(s2)){ // using ternary operator and checking if s1 equals to s2
+        System.out.println("strings are equal");
      }
      else{
-        System.out.println("Words are different");
+        System.out.println("strings are different");
      }
-    boolean alternate= s1.length() == s2.length();
+
+    boolean alternate= s1.length() == s2.length();//checking if both hava same length
     System.out.println("Does alternation possible? " + alternate);
 
-    StringBuilder alter = new StringBuilder();
-    for(int i=0; i<s1.length() && i<s2.length(); i++){
-        alter.append(s1.charAt(i)).append(s2.charAt(i));
+    StringBuilder alter = new StringBuilder();//creating empty StringBuilder tp build a new string in a loop
+    for(int i=0; i<s1.length() && i<s2.length(); i++){ //find the lenght of shorter string
+        alter.append(s1.charAt(i)).append(s2.charAt(i));// add s1 i character, the s2
     }
-    System.out.println("Alternating: "+alter);
+    System.out.println("Alternating: "+alter); // prints the final alternating
 
     obj.close();
-
-
 
     }
 }
